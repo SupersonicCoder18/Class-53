617b6cdedf2c8c8f7d2590d051f52643fe97ff95
Sravas Melarkode <63852545+SupersonicCoder18@users.noreply.github.com>
2020-06-04 16:57:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Button, View, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+        <view style = {{marginLeft:120, marginTop:350}}>
+        <Button color = "#FF4563" title = "Supersonic Coder!!!!" ></Button>
+        </view>
+    );
+  }
+}
